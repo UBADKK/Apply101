ee93ae07f9c27f5d41c48290834b37f308babbe9
umut-baris <umutbaris@sabanciuniv.edu>
2026-06-04 13:43:57 +0300

    db update

--- backend/routers/matches.py
+++ backend/routers/matches.py
@@ -15,7 +15,7 @@ router = APIRouter(
 
 MATCH_MODEL = "backend_rule_based"
 MATCH_PROMPT_VERSION = "backend_match_v1"
-MAX_BATCH_MATCH_JOBS = 50
+MAX_BATCH_MATCH_JOBS = 200
 
 
 def safe_json_loads(value, default):
